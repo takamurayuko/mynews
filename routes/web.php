2ef34dd7b11361709a93628a4902843c627f4ce4
takamura <res_55_9@yahoo.co.jp>
2023-03-14 06:01:00 +0000

    kadai18 commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+/* 3、http://XXXXXX.jp/XXXというアクセスが来た時に、
+AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
+*/
+//use App\Http://XXXXXX.jp/XXX;
+Route::controller(AAAController::class)->group(function()
+{
+    Route::get('XXX', 'bbb');
+});
+/*　4、前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
+web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の 
+add Action に、
+admin/profile/edit にアクセスしたら
+ProfileController の edit Action に割り当てるように設定してください
+*/
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+});
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('profile/edit', 'edit');
+});
