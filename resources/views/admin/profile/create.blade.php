8c45705290679b7f0334f69eb62bad3b9545116d
takamura <res_55_9@yahoo.co.jp>
2023-03-17 08:26:05 +0000

    kadai20 commit

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,10 +1,24 @@
 <!DOCTYPE html>
 <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Profile</title>
-        </head>
-        <body>
-            <h1>Name</h1>
-        </body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <body>
+    @extends('layouts.admin') 
+    
+    @section('title', 'Profile')
+   
+    @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Profile</h2>
+                    <p>Name:高村 祐子<p/>
+            </div>
+        </div>
+    </div>
+    @endsection
+        
+   </body>
 </html>
