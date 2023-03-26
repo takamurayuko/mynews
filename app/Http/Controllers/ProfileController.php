cc8659fc75ad8e5e5ca33a422ba6ba16b5c5d69a
takamura <res_55_9@yahoo.co.jp>
2023-03-26 10:37:57 +0000

    kadai29-02 commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,8 +8,15 @@ use App\Models\Profile;
 
 class ProfileController extends Controller
 {
-    public function index(Request $request)
+     public function index(Request $request)
     {
-        return view('profile.index');
+     $posts = Profile::all()->sortByDesc('updated_at');
+
+        if (count($posts) > 0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
+        }
+        return view('profile.index', ['headline' => $headline, 'posts' => $posts]);
     }
 }
